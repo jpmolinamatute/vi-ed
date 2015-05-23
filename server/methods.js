2e25a1f608aa3c5ff0787ff4175c978b82afaece
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2015-05-23 18:35:35 -0400

    OAuth1 pacakge was added some comments were removed

--- server/methods.js
+++ server/methods.js
@@ -1,17 +1,16 @@
 /* global elementsDB:false*/
+/* global sectionsDB:false*/
 Meteor.methods({
     elementUpsert: function (id, doc) {
         "use strict";
         check(id, Object);
         check(doc, Object);
-        console.log(id, doc);
         elementsDB.upsert(id, doc);
     },
     sectionUpsert: function (id, doc) {
         "use strict";
         check(id, Object);
         check(doc, Object);
-        console.log(id, doc);
         sectionsDB.upsert(id, doc);
     }
 });
