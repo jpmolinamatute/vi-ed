921854b32efbb9848f5471dd961b155b526749ad
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2015-05-31 01:13:04 -0400

    updating some externals libraries

--- server/startup.js
+++ server/startup.js
@@ -48,13 +48,13 @@ var accounts = [
 ];
 
 
-_.each(accounts, function (value) {
-    "use strict";
-
-    var service = Object.keys(value)[0];
-    var query = value[service][0];
-    var update = value[service][1];
-    var id = ServiceConfiguration.configurations.upsert(query, update, {upsert: true});
-    console.log(id);
-});
+//_.each(accounts, function (value) {
+//    "use strict";
+//
+//    var service = Object.keys(value)[0];
+//    var query = value[service][0];
+//    var update = value[service][1];
+//    var id = ServiceConfiguration.configurations.upsert(query, update, {upsert: true});
+//    console.log(id);
+//});
 
