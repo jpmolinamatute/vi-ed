29be37fb234978abebc72589e5fc2beeaf4d72d0
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2015-06-03 21:06:47 -0400

    working on Video options

--- both/collections.js
+++ both/collections.js
@@ -3,7 +3,11 @@
 /*global pagesDB: true*/
 /*global eDefaultsDB: true*/
 
+
 elementsDB = new Mongo.Collection("elements");
+
 sectionsDB = new Mongo.Collection("sections");
+
 pagesDB = new Mongo.Collection("pages");
+
 eDefaultsDB = new Mongo.Collection("elementDefault");
