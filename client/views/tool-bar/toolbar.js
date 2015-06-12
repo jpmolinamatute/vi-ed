9d16f77174e0fa83562300b9be2a62d2b06efd7e
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2015-06-11 21:33:23 -0400

    working on status bar, toolbar elements are implemented,  latitude and longitude are missing, update .gitignore

--- client/views/tool-bar/toolbar.js
+++ client/views/tool-bar/toolbar.js
@@ -2,15 +2,42 @@
 Template.toolbar.onRendered(function () {
     "use strict";
     var tools = this.$("div.tools");
+    var $filler = $("div#vied-statusbar-filler");
+    var $opt1 = $("div#vied-statusbar-opt1");
+    var $opt2 = $("div#vied-statusbar-opt2");
+
+
     tools.draggable({
         helper: "clone",
         revert: "invalid",
         zIndex: 100,
-        start: function () {
+        start: function (event, ui) {
+            var top = Math.round(ui.position.top * 100) / 100;
+            var left = Math.round(ui.position.left * 100) / 100;
             $("div#vied-main").css("background-color", "darkgrey");
+            $filler.css("width", "50%");
+            $opt1.css("display", "inline-block");
+            $opt2.css("display", "inline-block");
+            $opt1.find("span.tag").html("Top:");
+            $opt1.find("span.value").html(top + "px");
+            $opt2.find("span.tag").html("Left:");
+            $opt2.find("span.value").html(left + "px");
+        },
+        drag: function (event, ui) {
+            var top = Math.round(ui.position.top * 100) / 100;
+            var left = Math.round(ui.position.left * 100) / 100;
+            $opt1.find("span.value").html(top + "px");
+            $opt2.find("span.value").html(left + "px");
         },
         stop: function () {
             $("div#vied-main").css("background-color", "#6DBDD6");
+            $filler.width("100%");
+            $opt1.css("display", "none");
+            $opt2.css("display", "none");
+            $opt1.find("span.tag").html(" ");
+            $opt1.find("span.value").html(" ");
+            $opt2.find("span.tag").html(" ");
+            $opt2.find("span.value").html(" ");
         }
     });
 });
---
+++ server/updateDefaults.js
@@ -0,0 +1,66 @@
+/* global eDefaultsDB: false */
+
+
+//eDefaultsDB.remove({});
+//var newDefaults = [
+//    {
+//        "_id": "anchor",
+//        "data": {},
+//        "img": "fa-anchor",
+//        "label": "Anchor",
+//        "style": {"height": "50px", "width": "50px"},
+//        "type": "anchor"
+//    },
+//    {
+//        "_id": "image",
+//        "data": {},
+//        "img": "fa-picture-o",
+//        "label": "Image",
+//        "style": {"height": "300px", "width": "300px"},
+//        "type": "image"
+//    },
+//    {
+//        "_id": "map",
+//        "data": {"center": {"latitude": 43.70671711331573, "longitude": -79.34317783947336}, "markers": [], "zoom": 12},
+//        "img": "fa-map-marker",
+//        "label": "Map",
+//        "style": {"height": "200px", "width": "200px"},
+//        "type": "map"
+//    },
+//    {
+//        "_id": "shape",
+//        "data": {},
+//        "img": "fa-file-image-o",
+//        "label": "Shape",
+//        "style": {"height": "200px", "width": "200px"},
+//        "type": "shape"
+//    },
+//    {
+//        "_id": "texteditor",
+//        "data": {},
+//        "img": "fa-file-text-o",
+//        "label": "Text Editor",
+//        "style": {"height": "200px", "width": "500px"},
+//        "type": "texteditor"
+//    },
+//    {
+//        "_id": "video",
+//        "data": {
+//            "autohide": true,
+//            "autoplay": false,
+//            "control": true,
+//            "loop": false,
+//            "playlist": [],
+//            "videoID": "yKC6XmsLWVQ"
+//        },
+//        "img": "fa-video-camera",
+//        "label": "Video",
+//        "style": {"height": "270px", "width": "480px"},
+//        "type": "video"
+//    }
+//];
+//
+//_.each(newDefaults, function(value){
+//    "use strict";
+//    eDefaultsDB.insert(value);
+//});
