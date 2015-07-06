88bfb09a362f96579bd5d04ee92829201cdd0ec7
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2015-07-06 19:56:16 -0400

    working with AWS S3

--- server/init.js
+++ server/init.js
@@ -3,5 +3,5 @@
 S3.config = {
     key: process.env.AWS_ACCESS_KEY_ID,
     secret: process.env.AWS_SECRET_ACCESS_KEY,
-    bucket: process.env.S3_BUCKET_NAME
+    bucket: process.env.AWS_BUCKET_NAME
 };
